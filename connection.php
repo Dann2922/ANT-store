fe4d0bbc0e4033fc2028e842131acc3628653bdf
Dann2922 <tangtngcc200110@fpt.edu.vn>
2022-10-25 15:28:48 +0700

    h

--- connection.php
+++ connection.php
@@ -3,7 +3,7 @@
 	
 	mysqli_query($Connect,'SET NAMES "utf8"');
 	//mysqli_close($Connect);*/
-	$Connect = pg_connect("postgres://umykjuhgzkeoup:8468f695b3a2942d75e9c946888a9dc12157d251dbdf5728bd07e64490517f3a@ec2-54-173-237-110.compute-1.amazonaws.com:5432/ddj02tjkr0uhd8");
+	$Connect = pg_connect("postgres://qxwbmsttxkdgpx:7098075ea64caaebe50371619d5297227822ac17dccca9d9896a2c280ee410f2@ec2-3-226-163-72.compute-1.amazonaws.com:5432/d3jes7p6vik0pr");
     //$Connect = pg_connect("host=localhost port=5432 dbname=postgres");
 	//$Connect = pg_connect("host=localhost port=5432 dbname=postgres user=postgres password=123456");
 	
